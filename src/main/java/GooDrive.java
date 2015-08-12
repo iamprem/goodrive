aedb11552603254985d878fd4f323e339fd0c497
iamprem <mprem@outlook.com>
2015-08-12 02:46:07 -0400

    Drive > Local download without duplicate files

--- src/main/java/GooDrive.java
+++ src/main/java/GooDrive.java
@@ -1,11 +1,6 @@
 /**
  * Created by Prem on 8/8/15.
  */
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
@@ -14,38 +9,45 @@ import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.*;
 import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
 
-import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.nio.file.attribute.UserDefinedFileAttributeView;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class GooDrive {
 	/** Application name. */
-	private static final String APPLICATION_NAME = "GooDrive";
+	protected static final String APPLICATION_NAME = "GooDrive";
 
 	/** GooDrive home folder */
-	private static final String HOME_DIR = makeRootDir();
+	protected static final String HOME_DIR = makeRootDir();
 
 	/** Directory to store user credentials for this application. */
-	private static final java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"),
+	protected static final java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"),
 			".credentials/drive-api-quickstart");
 
 	/** Global instance of the {@link FileDataStoreFactory}. */
-	private static FileDataStoreFactory DATA_STORE_FACTORY;
+	protected static FileDataStoreFactory DATA_STORE_FACTORY;
 
 	/** Global instance of the JSON factory. */
-	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+	protected static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
 	/** Global instance of the HTTP transport. */
-	private static HttpTransport HTTP_TRANSPORT;
+	protected static HttpTransport HTTP_TRANSPORT;
 
 	/** Global instance of the scopes required by this quickstart. */
-	private static final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE);
+	protected static final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE);
 
 	static {
 		try {
@@ -55,36 +57,6 @@ public class GooDrive {
 			t.printStackTrace();
 			System.exit(1);
 		}
-	}
-
-	/**
-	 * Creates an authorized Credential object.
-	 * 
-	 * @return an authorized Credential object.
-	 * @throws IOException
-	 */
-	public static Credential authorize() throws IOException {
-		// Load client secrets.
-		InputStream in = GooDrive.class.getResourceAsStream("/client_secret.json");
-		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-		// Build flow and trigger user authorization request.
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-				clientSecrets, SCOPES).setDataStoreFactory(DATA_STORE_FACTORY).setAccessType("offline").build();
-		Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
-		System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-		return credential;
-	}
-
-	/**
-	 * Build and return an authorized Drive client service.
-	 * 
-	 * @return an authorized Drive client service
-	 * @throws IOException
-	 */
-	public static Drive getDriveService() throws IOException {
-		Credential credential = authorize();
-		return new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName(APPLICATION_NAME).build();
 	}
 
 	/**
@@ -104,37 +76,137 @@ public class GooDrive {
 		return file.getAbsolutePath();
 	}
 
-	public static void main(String[] args) throws IOException {
-		// Build a new authorized API client service.
-		Drive service = getDriveService();
+	/**
+	 * Download files and folders from Drive preserving the folder structure
+	 * 
+	 * TODO: Files with same name will be replaced with the last occurrence
+	 * while downloading/ Have to handle this like Google Drive windows client
+	 * 
+	 * @param service
+	 * @throws IOException
+	 */
+	public static void download(Drive service) throws IOException {
+		Stack<CurrentDirectory> dirLevel = new Stack<CurrentDirectory>();
+		// Initial Level as 'root'
+		dirLevel.push(new CurrentDirectory("root", HOME_DIR, "GooDrive"));
 
-		FileList result = service.files().list().setMaxResults(1000).execute();
-		List<File> files = result.getItems();
-		if (files == null || files.size() == 0) {
-			System.out.println("No files found.");
-		} else {
+		while (!dirLevel.isEmpty()) {
+			CurrentDirectory curDir = dirLevel.pop();
+			FileList result = service.files().list().setQ("'" + curDir.getId() + "' in parents").execute();
+			List<File> files = result.getItems();
 
-			System.out.println("Files:");
-			OutputStream os = null;
-			for (File file : files) {
-				
-				System.out.printf("%s (%s) - %s\n", file.getTitle(), file.getMimeType(), file.getDownloadUrl());
-				if (file.getDownloadUrl() != null) {
-					os = new FileOutputStream(new java.io.File(HOME_DIR +"/"+ file.getTitle()));
-					InputStream is = service.files().get(file.getId()).executeMediaAsInputStream();
-					int read = 0;
-					byte[] bytes = new byte[1024];
-					while ((read = is.read(bytes)) != -1) {
-						 os.write(bytes, 0, read);
+			if (files == null) {
+				System.out.println("Empty directory : " + curDir.getTitle());
+
+			} else {
+				OutputStream os = null;
+				for (File file : files) {
+
+					if (file.getDownloadUrl() != null) {
+						String filePath = curDir.getPath() + java.io.File.separator + file.getTitle();
+						java.io.File diskFile = new java.io.File(filePath);
+						Path path = diskFile.toPath();
+						if (diskFile.exists() && diskFile.isFile()) {
+							// TODO:Compare checksum
+							UserDefinedFileAttributeView view1 = Files.getFileAttributeView(path,
+									UserDefinedFileAttributeView.class);
+							String name = "md5CheckSum";
+							ByteBuffer buf = ByteBuffer.allocate(view1.size(name));
+							view1.read(name, buf);
+							buf.flip();
+							String value = Charset.defaultCharset().decode(buf).toString();
+							if (value.equals(file.getMd5Checksum())) {
+								// NOT MODIFIED
+								System.out.println(file.getTitle() + " -- NOT MODIFIED");
+							} else {
+								os = new FileOutputStream(diskFile);
+								InputStream is = service.files().get(file.getId()).executeMediaAsInputStream();
+								int read = 0;
+								byte[] bytes = new byte[1024];
+								while ((read = is.read(bytes)) != -1) {
+									os.write(bytes, 0, read);
+								}
+								os.close();
+								System.out.println(file.getTitle() + " - Recreated!");
+							}
+							System.out.println(value);
+						} else {
+							os = new FileOutputStream(diskFile);
+							InputStream is = service.files().get(file.getId()).executeMediaAsInputStream();
+							int read = 0;
+							byte[] bytes = new byte[1024];
+							while ((read = is.read(bytes)) != -1) {
+								os.write(bytes, 0, read);
+							}
+							os.close();
+							System.out.println(file.getTitle() + " - Done!");
+						}
+
+						UserDefinedFileAttributeView view = Files.getFileAttributeView(path,
+								UserDefinedFileAttributeView.class);
+						view.write("id", ByteBuffer.wrap(file.getId().getBytes()));
+						view.write("md5CheckSum", ByteBuffer.wrap(file.getMd5Checksum().getBytes()));
+						view.write("mimeType", ByteBuffer.wrap(file.getMimeType().getBytes()));
+						view.write("parents", ByteBuffer.wrap(file.getParents().toString().getBytes()));
+						FileTime ft = FileTime.fromMillis(file.getModifiedDate().getValue());
+						Files.setLastModifiedTime(path, ft);
+						// ft =
+						// FileTime.fromMillis(file.getCreatedDate().getValue());
+						// Files.setAttribute(path, "creationTime", ft);
+
+						// To Print
+						BasicFileAttributes attr = Files.readAttributes(path, BasicFileAttributes.class);
+						System.out.println("creationTime: " + attr.creationTime());
+						System.out.println("lastModifiedTime: " + attr.lastModifiedTime());
+						UserDefinedFileAttributeView view1 = Files.getFileAttributeView(path,
+								UserDefinedFileAttributeView.class);
+						String name = "md5CheckSum";
+						ByteBuffer buf = ByteBuffer.allocate(view1.size(name));
+						view1.read(name, buf);
+						buf.flip();
+						String value = Charset.defaultCharset().decode(buf).toString();
+						System.out.println(value);
+
+					} else {
+
+						String dirPath = curDir.getPath() + java.io.File.separator + file.getTitle();
+						java.io.File dir = new java.io.File(dirPath);
+						if (!dir.exists()) {
+							dir.mkdirs();
+						}
+						CurrentDirectory newDir = new CurrentDirectory(file.getId(), dirPath, file.getTitle());
+						dirLevel.push(newDir);
+						Path path = dir.toPath();
+						UserDefinedFileAttributeView userView = Files.getFileAttributeView(path,
+								UserDefinedFileAttributeView.class);
+						FileTime ft = FileTime.fromMillis(file.getModifiedDate().getValue());
+						Files.setLastModifiedTime(path, ft);
+						userView.write("id", ByteBuffer.wrap(file.getId().getBytes()));
+						userView.write("mimeType", ByteBuffer.wrap(file.getMimeType().getBytes()));
+						// TODO: Drive can have multiple parents
+						// TODO: After creating the folder if we add files to
+						// that, modified date changes. :P Expected!!!
+						userView.write("parents", ByteBuffer.wrap(file.getParents().toString().getBytes()));
 					}
-					System.out.println(file.getTitle() + " - Done!");
-				} else{
-					java.io.File dir = new java.io.File(HOME_DIR+"/"+file.getTitle());
-					dir.mkdirs();
+
 				}
 
 			}
+
 		}
+
+	}
+
+	/**
+	 * Starting point of the program
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		// Build a new authorized API client service.
+		Drive service = Authentication.getDriveService();
+		download(service);
+
 	}
 
 }
